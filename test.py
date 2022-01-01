0f673d2cfc128759293b486a0d4082fae4608177
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-01 17:15:27 +0000

    Playlist Selection

--- test.py
+++ test.py
@@ -1,22 +1,37 @@
+from decouple import config
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
+CLIENT_ID=config('CLIENT_ID')
+CLIENT_SECRET=config('CLIENT_SECRET')
+REDIRECT_URI=config('REDIRECT_URI')
+
+scope = "user-library-read playlist-read-private"
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope=scope,
-        client_id="",
-        client_secret="",
-        redirect_uri="http://localhost:8080/",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
     )
 )
 
 # results = sp.current_user_saved_tracks()
 
-results = sp.current_user_saved_tracks()
+results = sp.current_user_playlists()
 
-print(results)
+#print(results)
+#print(results["items"])
 
+print ("SELECT PLAYLIST")
+print ("---------------")
+for count, playlist_results in enumerate(results['items']):
+    # print (count)
+    # print (playlist_results)
+    print (str(count) +": "+ playlist_results['name'])
+
+playlist_selection = int(input("Enter playlist number: "))
+playlist_id = results['items'][playlist_selection]['id']
 # for idx, item in enumerate(results["items"]):
 #     track = item["track"]
 # print(idx, track["artists"][0]["name"], " - ", track["name"])
