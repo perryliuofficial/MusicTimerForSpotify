e47ee925821e1466c6b46ee85db0012ed4ba428c
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-01 15:39:17 +0000

    ahhhhhhhhhhhhhhhhhhhhh

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # SpotifyMusicTimer
 Timer that plays songs for a given length of time from spotify
 
-```
-pip install spotipy
-pip3 install spotipy Flask Flask-Session
-pip install python-decouple
-```
+1. ask user for timer length
+2. get user's playlists https://developer.spotify.com/console/get-current-user-playlists/?limit=&offset=, returns a json file
+3. from there users can select a playlist (we'll grab the playlist id from json)
+4. get the list of songs in playlist https://developer.spotify.com/console/get-playlist-tracks/
+5. sort them by duration_ms
+6. find right song
+7. add song to playback queue (so we have to assume user has nothing in queue for now, since we cannot clear the queue) https://developer.spotify.com/console/post-queue/
+8. start playback https://developer.spotify.com/console/put-play/
+9. stop playback when timer is up https://developer.spotify.com/console/put-pause/
--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import os
+import sys
+import json
+import webbrowser
 from decouple import config
 from flask import Flask, session, request, redirect
 from flask_session import Session
+from speech_recognition import Microphone, Recognizer, UnknownValueError
 import spotipy
+import spotipy.util as util
 import uuid 
 import requests
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
-
+from json.decoder import JSONDecodeError
+import pandas as pd
+import pprint
+from time import sleep
 
 # These are stored within .env, which is gitignored.
-CLIENT_ID=config('CLIENT_ID')
-CLIENT_SECRET=config('CLIENT_SECRET')
-REDIRECT_URI=config('REDIRECT_URI')
+SPOTIPY_CLIENT_ID=config('CLIENT_ID')
+SPOTIPY_CLIENT_SECRET=config('CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI=config('REDIRECT_URI')
 # -----------------------------------------------
 # The contents of the .env file should look like this:
 # CLIENT_ID=000000000000000000000000
