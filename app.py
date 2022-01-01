48fbacd9b77babe1131833549cbfb411553da96c
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-01 15:57:58 +0000

    ahhhhhhhhhhhhhhhhhhhhh

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import spotipy
 import spotipy.util as util
 import uuid 
 import requests
+import base64
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 from json.decoder import JSONDecodeError
 import pandas as pd
@@ -17,11 +18,53 @@ import pprint
 from time import sleep
 
 # These are stored within .env, which is gitignored.
-SPOTIPY_CLIENT_ID=config('CLIENT_ID')
-SPOTIPY_CLIENT_SECRET=config('CLIENT_SECRET')
-SPOTIPY_REDIRECT_URI=config('REDIRECT_URI')
+CLIENT_ID=config('CLIENT_ID')
+CLIENT_SECRET=config('CLIENT_SECRET')
+REDIRECT_URI=config('REDIRECT_URI')
 # -----------------------------------------------
 # The contents of the .env file should look like this:
 # CLIENT_ID=000000000000000000000000
 # CLIENT_SECRET=000000000000000000000000
 # REDIRECT_URI='http://google.com'
+
+# Step 1 - Authorization 
+url = "https://accounts.spotify.com/api/token"
+headers = {}
+data = {}
+
+# Encode as Base64
+message = f"{CLIENT_ID}:{CLIENT_SECRET}"
+messageBytes = message.encode('ascii')
+base64Bytes = base64.b64encode(messageBytes)
+base64Message = base64Bytes.decode('ascii')
+
+headers['Authorization'] = f"Basic {base64Message}"
+data['grant_type'] = "client_credentials"
+
+r = requests.post(url, headers=headers, data=data)
+
+token = r.json()['access_token']
+
+# Step 2 - Get Current User's Playlists
+# playLists = f"https://api.spotify.com/v1/me/playlists"
+# headers = {
+#     "Authorization": "Bearer " + token
+# }
+
+# res = requests.get(
+#     url=playLists,
+#     headers=headers)
+
+# print(json.dumps(res.json(), indent=2))
+
+# Step X - Use Access Token to call playlist endpoint
+
+playlistId = "37i9dQZF1DWZeKCadgRdKQ"
+playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}"
+headers = {
+    "Authorization": "Bearer " + token
+}
+
+res = requests.get(url=playlistUrl, headers=headers)
+
+print(json.dumps(res.json(), indent=2))
