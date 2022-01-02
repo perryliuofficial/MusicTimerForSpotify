cab42ad76e0dbc2c9a0f805ab66b5ce75a9e8a49
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-02 21:27:48 +0000

    IT WORKS OMFG

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
 sp = 0
-playlist_id = ""
+playlist_id = str
+timer = float
+timerPlaylistId = str
+user_id = str
 app = Flask(__name__)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, Response, redirect, url_for, flash
-from app import app, sp
+from app import app, sp, playlist_id, timer, timerPlaylistId, user_id
 from decouple import config
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from app.forms import InputForm, PlaylistForm
 import json
+import time
 
 app.config['SECRET_KEY']=config('FLASK_WTF_KEY') #Flask WTF key
 
@@ -37,6 +38,7 @@ def timer():
     #################################################################
     # Get user ID
     results = sp.current_user()
+    global user_id
     user_id = results["id"]
 
     #################################################################
@@ -45,7 +47,7 @@ def timer():
     playlist_items = results["items"]
 
     timerPlaylistExists = False
-    timerPlaylistId: str
+    global timerPlaylistId
 
     for item in playlist_items:
         name = item["name"]
@@ -74,7 +76,6 @@ def timer():
         sp.playlist_remove_all_occurrences_of_items(playlist_id=timerPlaylistId, items=trackIds)
 
     clearPlaylist()
-
     form = InputForm()
     if request.method == 'POST':
         timer = form.timer.data
@@ -87,7 +88,7 @@ def timer():
 @app.route('/playlist',methods=['POST', 'GET'])
 def playlist():
     stage = 3
-
+    global timer
     timer = request.form.get('timer')
     
     #################################################################
@@ -126,6 +127,70 @@ def playlistRoute():
 def play():
     stage = 4
     global playlist_id
+    global timer
+    timer = float(timer)*60000 # convert from minutes to ms
     playlist_id = request.args.get('playlist_id')
-    print(playlist_id)
+    #################################################################
+    # Select Song
+    global sp
+    results = sp.playlist_tracks(playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=('track', ))
+    # Find song length that is closest to given timer
+    difference_to_timer = float(abs(results['items'][0]['track']['duration_ms'] - float(timer)))   
+    play_track_index = 0
+    for count, track_results in enumerate(results['items']):
+        current_track_difference = float(abs(track_results['track']['duration_ms'] - float(timer)))
+        print(str(track_results['track']['duration_ms'])+" - "+str(timer)+" = "+str(current_track_difference))
+        print(float(abs(track_results['track']['duration_ms'] - float(timer))))
+        if current_track_difference < difference_to_timer:
+            play_track_index = count
+            difference_to_timer = current_track_difference
+
+    play_track_id = [results['items'][play_track_index]['track']['uri']]
+    playlist_length = results['items'][play_track_index]['track']['duration_ms']
+
+    songs_in_playlist = int(results['total'])-1
+    while float(timer) > float(playlist_length) and float(timer) - float(playlist_length) > 15000: #15 seconds
+        if play_track_index == songs_in_playlist:
+            play_track_index = 0
+        else:
+            play_track_index += 1
+        play_track_id.append(results['items'][play_track_index]['track']['uri'])
+        playlist_length += float(results['items'][play_track_index]['track']['duration_ms'])
+
+    #################################################################
+    # add the song(s) we want to the playlist
+    global timerPlaylistId
+    global user_id
+    sp.user_playlist_add_tracks(user_id, playlist_id=timerPlaylistId, tracks=play_track_id)
+
+    #################################################################
+    # Play Song
+    playlistUri = "spotify:playlist:" + timerPlaylistId
+    sp.start_playback(context_uri=playlistUri)
+
+    # Start Timer
+    time.sleep(float(timer)/1000) #convert ms to seconds
+
+    # When time is up, stop current song and play song ID 1tFL456Lotpvnk8gCfZQOQ
+    sp.pause_playback(device_id=None)
+
+    def clearPlaylist():
+        current_timer_playlist_songs = sp.playlist_items(playlist_id=timerPlaylistId, fields="items(track(id))")
+        trackIds = list()
+        for track in current_timer_playlist_songs["items"]:
+            trackIds.append(track["track"]["id"])
+        sp.playlist_remove_all_occurrences_of_items(playlist_id=timerPlaylistId, items=trackIds)
+
+    clearPlaylist()
+    play_track_id.clear()
+    play_track_id = ['spotify:track:1tFL456Lotpvnk8gCfZQOQ']
+    sp.user_playlist_add_tracks(user_id, playlist_id=timerPlaylistId, tracks=play_track_id)
+    playlistUri = "spotify:playlist:" + timerPlaylistId
+    sp.start_playback(context_uri=playlistUri)
+
+    # Stop alarm after 10 seconds
+    time.sleep(10)
+    sp.pause_playback(device_id=None)
+    clearPlaylist()
+
     return render_template('play.html', title='Music Timer for Spotify', playlist_id=playlist_id, timer=timer)
